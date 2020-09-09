66b7c029eb2cd929e6cbea206c1b92135775123a
Travischeung <travis.cheung69@gmail.com>
2020-09-09 16:57:34 -0400

    should work now

--- theBot.py
+++ theBot.py
@@ -8,18 +8,14 @@ userData = json.loads(f.read())
 print(userData.get("username"))
 print(userData.get("password"))
 # hopefully this will sign me in
-currSession = InstaPy(username = userData.get("username"), password = userData.get("password"),headless_browser=False)
-
+# session = InstaPy(username = userData.get("username"), password = userData.get("password"))
+session = InstaPy(username=userData.get("username"),
+                  password=userData.get("password"),
+                  geckodriver_path='C:\\Users\\Lok PC\\Downloads\\geckodriver-v0.27.0-win64\\geckodriver.exe')
+# session.login()
 with smart_run(session):
     # begin the epic motivation
-    session.set_do_follow(enabled=True, percentage=50)
+    session.set_do_follow(enabled=True, percentage=100)
     session.set_comments(["follow me for epic tips to be quintillionaire"])
     session.set_do_comment(enabled=True, percentage=100)
-    session.interact_by_users(['chilltravischill'])
-
-
-    # session.set_do_follow(enabled=False, percentage=50)
-    # session.set_comments(["Cool", "Super!"])
-    # session.set_do_comment(enabled=True, percentage=80)
-    # session.set_do_like(True, percentage=70)
-    # session.interact_by_users(['user1', 'user2', 'user3'], amount=5, randomize=True, media='Photo')
+    session.interact_by_users(['ishaucerous'],amount=5,randomize=False,media="Photo")
